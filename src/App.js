cd6a7cfed62be7085239c2582d0fa87a79ba51e6
aleksandrapanasuk <apanasHD2@gmail.com>
2023-12-18 08:34:32 +0300

    target event

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import React, {useState} from "react";
 function App() {
 
       const[likes, setLikes]=useState(5);
+      const [value,setValue]=useState('text input')
       function increment(){
           setLikes(likes+1)
       }
@@ -13,6 +14,10 @@ function App() {
     <center>
         <div className={App}>
             <h1>{likes}</h1>
+            <h2>{value}</h2>
+            <input type="text" value={value} onChange={event => setValue(event.target.value)}/>
+            <br/>
+            <hr/>
             <button onClick={increment}>like</button>
             <button onClick={decriment}>dislike</button>
 
